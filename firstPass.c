09e6fe0cac043dff17fe4fc0a5626859203084b3
Ilai261 <ilai.bagab@gmail.com>
2021-07-21 21:39:18 +0300

    Update firstPass.c

--- firstPass.c
+++ firstPass.c
@@ -16,7 +16,21 @@ firstPass(FILE* fp,label* labels, int* IC, int* DC)
 		else
 		{
 			line[lineCount] = '\0';
+			int i = 0;
+			bool isEmptyLine = true;
+			char firstChar = ' ';
 			
+			for(i; i < LineCount; i++)
+			{
+				if(!isspace(line[i]))
+					isEmptyLine = false;
+					firstChar = line[i];
+					break;
+			}
+			if(isEmptyLine == false && firstChar != ';')
+			{
+				
+			}
 			/*first we will check if the line is empty or a comment, then we will check if is starts with a label*/
 			
 		}
