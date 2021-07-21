a4965b9ec4b035028cb3ce117c7e21a7f0eeb474
Ilai261 <ilai.bagab@gmail.com>
2021-07-21 20:06:40 +0300

    Update firstPass.c

--- firstPass.c
+++ firstPass.c
@@ -4,24 +4,19 @@
 
 firstPass(FILE* fp,label* labels, int* IC, int* DC)
 {
-	char* line = (char*)calloc(1, 10);
-	line[10] = '\0';
+	char* line = char[81];
 	int lineCount = 0;
 	while((c = getc(fp)) != EOF)
 	{
 		if(c != '\n')
 		{
-			if(line[lineCount + 1] == '\0')
-			{
-				line = (char*)realloc(line, lineCount + 10);
-				line[lineCount + 10] = '\0';
-			}
-			line[lineCount] = c;
-			lineCount++;
+				line[lineCount] = c;
+				lineCount++;
 		}
 		else
 		{
-			/*first we will chack if the line is empty or a comment, then we will chack if is starts with a label*/
+			line[lineCount + 1] = '\0';
+			/*first we will check if the line is empty or a comment, then we will check if is starts with a label*/
 			
 		}
 	}
