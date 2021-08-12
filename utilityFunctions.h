3d190cace905b7a4abb1a658e0af81efc4b5c300
Ilai261 <ilai.bagab@gmail.com>
2021-08-12 17:39:05 +0300

    Update utilityFunctions.h

--- utilityFunctions.h
+++ utilityFunctions.h
@@ -5,7 +5,6 @@ int labelNum(label* labels,int labelCount, char* string);
 int isGuidance(char* string);
 void writeDataFromGuidance(int guidanceNum,unsigned char** dataArray,int *DC,char* dataString);
 int intPow(int base, int exp);
-int writeToBits(unsigned int * placeToWrite,int startBit, int endBit, int data );
 int scanStrAndMove(char **readString, char* formatString, char * writeString);
 int scanIntAndMove(char **readString, char* formatString, int * writeInt);
 int numOfDigits(int x);
